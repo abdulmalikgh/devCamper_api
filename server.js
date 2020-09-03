9b46505fad93e6454ffbb9b01092082771a20276
= <musah.abdulmalikgh@gmail.com>
2020-09-02 13:56:48 -1200

    Add data

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
 const app = express();
 const connectDB = require("./config/db.js");
+const colors = require("colors");
 // load router files
 const bootcamp = require("./routes/bootcamp");
 // load env configurations
@@ -18,13 +18,14 @@ const server = app.listen(
 	PORT,
 	console.log(
 		`Server is running in ${process.env.NODE_ENV} mode on port ${PORT} `
+			.yellow.bold
 	)
 );
 
 // handle unhandled rejection
 
 process.on("unhandledRejection", (err, promise) => {
-	console.log(`Error: ${err.message}`);
+	console.log(`Error: ${err.message}`.red);
 
 	server.close(() => process.exit(1));
 });
