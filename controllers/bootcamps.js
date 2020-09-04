85329949120278651ba0d1b972a20b340fa282f9
= <musah.abdulmalikgh@gmail.com>
2020-09-03 12:29:02 -1200

    Delete bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -42,39 +42,68 @@ exports.getBootcamp = async (req, res, next) => {
 //@route POST /api/v1/bootcamps
 //@access private
 exports.createBootcamp = async (req, res, next) => {
-	const bootcamp = await Bootcamp.create(req.body);
+	try {
+		const bootcamp = await Bootcamp.create(req.body);
 
-	res.status(201).json({
-		success: true,
-		data: bootcamp,
-	});
+		res.status(201).json({
+			success: true,
+			count: bootcamp.count,
+			data: bootcamp,
+		});
+	} catch (error) {
+		res.status(400).json({
+			success: false,
+		});
+	}
 };
 
 //@desc Update a bootcamp
 //@route PUT /api/v1/bootcamps
 //@access private
 exports.updateBootcamp = async (req, res, next) => {
-	const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-		new: true,
-		runValidators: true,
-	});
+	try {
+		const bootcamp = await Bootcamp.findByIdAndUpdate(
+			req.params.id,
+			req.body,
+			{
+				new: true,
+				runValidators: true,
+			}
+		);
 
-	if (!bootcamp) {
-		return res.status(400).json({ success: false });
+		if (!bootcamp) {
+			return res.status(400).json({ success: false });
+		}
+
+		res.status(200).json({
+			success: true,
+			data: bootcamp,
+		});
+	} catch (error) {
+		res.status(400).json({
+			success: bootcamp,
+		});
 	}
-
-	res.status(200).json({
-		success: true,
-		data: bootcamp,
-	});
 };
 
 //@desc delete a bootcamp
 //@route DELETE /api/v1/bootcamps
 //@access private
-exports.deleteBootcamp = (req, res, next) => {
-	res.status(200).json({
-		success: true,
-		msg: `Bootcamp deleted ${req.param.id}`,
-	});
+exports.deleteBootcamp = async (req, res, next) => {
+	try {
+		const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+
+		if (!bootcamp) {
+			return res.status(400).json({ success: false });
+		}
+
+		res.status(200).json({
+			success: true,
+			data: {},
+		});
+	} catch (error) {
+		res.status(400).json({
+			success: false,
+		});
+	}
 };
